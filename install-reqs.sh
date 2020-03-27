37f5608b157d0b4e9ecc130b52ff121dd827c5f9
Zhuo Zhen <zhenz@uchicago.edu>
2020-03-27 16:16:32 -0500

    add install-reqs and cuda

---
+++ install-reqs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root."
+  exit 1
+fi
+
+### OS-specific installs
+
+source /etc/os-release
+dnf install -y epel-release
+dnf install -y qemu-img python3-pip kpartx
+
+### Generic installs
+
+pip install diskimage-builder
